87e93d2ee57a601bfab4dd4304db0bc620a64141
ArtInLines <linesinart@gmail.com>
2023-10-23 13:58:12 +0200

    fix: naming

--- ail_alloc.h
+++ ail_alloc.h
@@ -1,9 +1,9 @@
 // Different types of Allocators
 //
 // Define AIL_ALLOC_IMPL in some file, to include the function bodies
-// Drop-in replacement for malloc/free via the global alloc_ctx variable
-// Call alloc_ctxAlloc, alloc_ctxFree, alloc_ctxFreeAll to use whichever allocator is currently set as the context
-// Use ALLOC_SWITCH_CTX and ALLOC_SWITCH_BACK_CTX to change the context (back)
+// Drop-in replacement for malloc/free via the global ail_alloc_ctx variable
+// Call ail_alloc_ctx_alloc, ail_alloc_ctx_free, ail_alloc_ctx_free_all to use whichever allocator is currently set as the context
+// Use AIL_ALLOC_SWITCH_CTX and AIL_ALLOC_SWITCH_BACK_CTX to change the context (back)
 
 #ifndef AIL_ALLOC_H_
 #define AIL_ALLOC_H_
@@ -13,56 +13,52 @@
 #endif // AIL_TYPES_IMPL
 #include "ail.h"
 
-#define ALLOC_UNLIKELY(expr) __builtin_expect(!!(expr), 0)
-#define ALLOC_LIKELY(expr)   __builtin_expect(!!(expr), 1)
-#define ALLOC_MAX(a, b)      (((a) > (b)) ? (a) : (b))
-
 typedef struct {
 	void *data;
 	void *(*alloc)(void*, size_t);
 	void *(*realloc)(void*, void*, size_t);
     void  (*free)(void*, void*);
     void  (*freeAll)(void*);
-} Alloc_Allocator;
+} AIL_Alloc_Allocator;
 
 // @TODO: This allocator isn't really an arena -> need another name
 typedef struct {
 	size_t cap;
 	size_t idx;
 	void  *next;
-	Alloc_Allocator allocator;
+	AIL_Alloc_Allocator allocator;
 } Alloc_Arena_Header;
 
-typedef size_t Alloc_Arena_El_Header;
+typedef size_t AIL_Alloc_Arena_El_Header;
 
-void *alloc_ctxAlloc(size_t size);
-void *alloc_ctxCalloc(size_t nelem, size_t elsize);
-void *alloc_ctxRealloc(void *ptr, size_t size);
-void alloc_ctxFree(void *ptr);
-void alloc_ctxFreeAll();
-void *alloc_stdAlloc(void *data, size_t size);
-void *alloc_stdRealloc(void *data, void *ptr, size_t size);
-void alloc_stdFree(void *data, void *ptr);
-void alloc_stdFreeAll(void *data);
-void *alloc_arenaNextBlock(Alloc_Allocator allocator, size_t size, bool growable);
-void *alloc_arenaAlloc(void *data, size_t size);
-void *alloc_arenaRealloc(void *data, void *ptr, size_t size);
-void alloc_arenaFree(void *data, void *ptr);
-void alloc_arenaFreeAll(void *data);
+void *ail_alloc_ctx_alloc(size_t size);
+void *ail_alloc_ctx_calloc(size_t nelem, size_t elsize);
+void *ail_alloc_ctx_realloc(void *ptr, size_t size);
+void ail_alloc_ctx_free(void *ptr);
+void ail_alloc_ctx_free_all();
+void *ail_alloc_std_alloc(void *data, size_t size);
+void *ail_alloc_std_realloc(void *data, void *ptr, size_t size);
+void ail_alloc_std_free(void *data, void *ptr);
+void ail_alloc_std_free_all(void *data);
+void *ail_alloc_arena_next_block(AIL_Alloc_Allocator allocator, size_t size, bool growable);
+void *ail_alloc_arena_alloc(void *data, size_t size);
+void *ail_alloc_arena_realloc(void *data, void *ptr, size_t size);
+void ail_alloc_arena_free(void *data, void *ptr);
+void ail_alloc_arena_free_all(void *data);
 
-static Alloc_Allocator alloc_std = {
+static AIL_Alloc_Allocator ail_alloc_std = {
 	.data    = NULL,
-	.alloc   = alloc_stdAlloc,
-	.realloc = alloc_stdRealloc,
-	.free    = alloc_stdFree,
-	.freeAll = alloc_stdFreeAll,
+	.alloc   = ail_alloc_std_alloc,
+	.realloc = ail_alloc_std_realloc,
+	.free    = ail_alloc_std_free,
+	.freeAll = ail_alloc_std_free_all,
 };
 
-static Alloc_Allocator *alloc_ctx = &alloc_std;
-static Alloc_Allocator *_alloc_prev_ctx_ = NULL;
-#define ALLOC_SWITCH_CTX(allocator) { _alloc_prev_ctx_ = alloc_ctx; alloc_ctx = &(allocator); }
-#define ALLOC_SWITCH_BACK_CTX() alloc_ctx = _alloc_prev_ctx_
-#define ALLOC_GET_HEADER(strct, data) ((strct *)(((char *)(data))-sizeof(strct)))
+static AIL_Alloc_Allocator *ail_alloc_ctx = &ail_alloc_std;
+static AIL_Alloc_Allocator *_ail_alloc_prev_ctx_ = NULL;
+#define AIL_ALLOC_SWITCH_CTX(allocator) { _ail_alloc_prev_ctx_ = ail_alloc_ctx; ail_alloc_ctx = &(allocator); }
+#define AIL_ALLOC_SWITCH_BACK_CTX() ail_alloc_ctx = _ail_alloc_prev_ctx_
+#define AIL_ALLOC_GET_HEADER(strct, data) ((strct *)(((char *)(data))-sizeof(strct)))
 
 
 #endif // AIL_ALLOC_H_
@@ -78,29 +74,29 @@ static Alloc_Allocator *_alloc_prev_ctx_ = NULL;
 // Context //
 /////////////
 
-void *alloc_ctxAlloc(size_t size)
+void *ail_alloc_ctx_alloc(size_t size)
 {
-	return alloc_ctx->alloc(alloc_ctx->data, size);
+	return ail_alloc_ctx->alloc(ail_alloc_ctx->data, size);
 }
 
-void *alloc_ctxCalloc(size_t nelem, size_t elsize)
+void *ail_alloc_ctx_calloc(size_t nelem, size_t elsize)
 {
-	return alloc_ctxAlloc(nelem * elsize);
+	return ail_alloc_ctx_alloc(nelem * elsize);
 }
 
-void *alloc_ctxRealloc(void *ptr, size_t size)
+void *ail_alloc_ctx_realloc(void *ptr, size_t size)
 {
-	return alloc_ctx->realloc(alloc_ctx->data, ptr, size);
+	return ail_alloc_ctx->realloc(ail_alloc_ctx->data, ptr, size);
 }
 
-void alloc_ctxFree(void *ptr)
+void ail_alloc_ctx_free(void *ptr)
 {
-	alloc_ctx->free(alloc_ctx->data, ptr);
+	ail_alloc_ctx->free(ail_alloc_ctx->data, ptr);
 }
 
-void alloc_ctxFreeAll()
+void ail_alloc_ctx_free_all()
 {
-	alloc_ctx->freeAll(alloc_ctx->data);
+	ail_alloc_ctx->freeAll(ail_alloc_ctx->data);
 }
 
 
@@ -108,25 +104,25 @@ void alloc_ctxFreeAll()
 // Std //
 /////////
 
-void *alloc_stdAlloc(void *data, size_t size)
+void *ail_alloc_std_alloc(void *data, size_t size)
 {
 	(void)data;
 	return malloc(size);
 }
 
-void *alloc_stdRealloc(void *data, void *ptr, size_t size)
+void *ail_alloc_std_realloc(void *data, void *ptr, size_t size)
 {
 	(void)data;
 	return realloc(ptr, size);
 }
 
-void alloc_stdFree(void *data, void *ptr)
+void ail_alloc_std_free(void *data, void *ptr)
 {
 	(void)data;
 	free(ptr);
 }
 
-void alloc_stdFreeAll(void *data)
+void ail_alloc_std_free_all(void *data)
 {
 	(void)data;
 }
@@ -136,75 +132,75 @@ void alloc_stdFreeAll(void *data)
 // Arena //
 ///////////
 
-Alloc_Allocator alloc_arenaInit(Alloc_Allocator allocator, size_t initialSize, bool growable)
+AIL_Alloc_Allocator ail_alloc_arena_init(AIL_Alloc_Allocator allocator, size_t initialSize, bool growable)
 {
-	return (Alloc_Allocator) {
-		.data    = alloc_arenaNextBlock(allocator, initialSize, growable),
-		.alloc   = alloc_arenaAlloc,
-		.realloc = alloc_arenaRealloc,
-		.free    = alloc_arenaFree,
-		.freeAll = alloc_arenaFreeAll,
+	return (AIL_Alloc_Allocator) {
+		.data    = ail_alloc_arena_next_block(allocator, initialSize, growable),
+		.alloc   = ail_alloc_arena_alloc,
+		.realloc = ail_alloc_arena_realloc,
+		.free    = ail_alloc_arena_free,
+		.freeAll = ail_alloc_arena_free_all,
 	};
 }
 
-void *alloc_arenaNextBlock(Alloc_Allocator allocator, size_t size, bool growable)
+void *ail_alloc_arena_next_block(AIL_Alloc_Allocator allocator, size_t size, bool growable)
 {
 	void *data = allocator.alloc(allocator.data, sizeof(Alloc_Arena_Header) + size);
 	*((Alloc_Arena_Header *)data) = (Alloc_Arena_Header) {
 		.cap  = size,
 		.idx  = 0,
 		.next = NULL,
-		.allocator = growable ? allocator : (Alloc_Allocator){0},
+		.allocator = growable ? allocator : (AIL_Alloc_Allocator){0},
 	};
 	return ((char *)(data)) + sizeof(Alloc_Arena_Header);
 }
 
-void *alloc_arenaAlloc(void *data, size_t size)
+void *ail_alloc_arena_alloc(void *data, size_t size)
 {
-	Alloc_Arena_Header *header = ALLOC_GET_HEADER(Alloc_Arena_Header, data);
-	if (ALLOC_UNLIKELY(header->idx + size + sizeof(Alloc_Arena_El_Header) >= header->cap)) {
+	Alloc_Arena_Header *header = AIL_ALLOC_GET_HEADER(Alloc_Arena_Header, data);
+	if (AIL_UNLIKELY(header->idx + size + sizeof(AIL_Alloc_Arena_El_Header) >= header->cap)) {
 		if (header->allocator.alloc) {
 			if (!header->next) {
-				header->next = alloc_arenaNextBlock(header->allocator, ALLOC_MAX(header->cap, 2*size), true);
+				header->next = ail_alloc_arena_next_block(header->allocator, AIL_MAX(header->cap, 2*size), true);
 			}
-			return alloc_arenaAlloc(header->next, size);
+			return ail_alloc_arena_alloc(header->next, size);
 		} else {
-			alloc_arenaFreeAll(data);
+			ail_alloc_arena_free_all(data);
 		}
 	}
-	void *out = &((char *)data)[header->idx + sizeof(Alloc_Arena_El_Header)];
-	header->idx += size + sizeof(Alloc_Arena_El_Header);
+	void *out = &((char *)data)[header->idx + sizeof(AIL_Alloc_Arena_El_Header)];
+	header->idx += size + sizeof(AIL_Alloc_Arena_El_Header);
 	return out;
 }
 
-void *alloc_arenaRealloc(void *data, void *ptr, size_t size)
+void *ail_alloc_arena_realloc(void *data, void *ptr, size_t size)
 {
-	if (!ptr) return alloc_arenaAlloc(data, size);
-	Alloc_Arena_Header    *header = ALLOC_GET_HEADER(Alloc_Arena_Header, data);
-	Alloc_Arena_El_Header *elSize = ALLOC_GET_HEADER(Alloc_Arena_El_Header, ptr);
-	while (ALLOC_UNLIKELY(((char *)(data)) + header->cap < ((char *)(ptr)))) header = ALLOC_GET_HEADER(Alloc_Arena_Header, data);
+	if (!ptr) return ail_alloc_arena_alloc(data, size);
+	Alloc_Arena_Header    *header = AIL_ALLOC_GET_HEADER(Alloc_Arena_Header, data);
+	AIL_Alloc_Arena_El_Header *elSize = AIL_ALLOC_GET_HEADER(AIL_Alloc_Arena_El_Header, ptr);
+	while (AIL_UNLIKELY(((char *)(data)) + header->cap < ((char *)(ptr)))) header = AIL_ALLOC_GET_HEADER(Alloc_Arena_Header, data);
 	if (header->idx == (size_t)(ptr) + *elSize) {
 		header->idx = (size_t)(ptr) + size;
 		*elSize     = size;
 		return ptr;
 	} else {
-		void *newptr = alloc_arenaAlloc(data, size);
+		void *newptr = ail_alloc_arena_alloc(data, size);
 		memcpy(ptr, newptr, *elSize);
 		return newptr;
 	}
 }
 
-void alloc_arenaFree(void *data, void *ptr)
+void ail_alloc_arena_free(void *data, void *ptr)
 {
 	(void)data;
 	(void)ptr;
 }
 
-void alloc_arenaFreeAll(void *data)
+void ail_alloc_arena_free_all(void *data)
 {
-	Alloc_Arena_Header *header = ALLOC_GET_HEADER(Alloc_Arena_Header, data);
+	Alloc_Arena_Header *header = AIL_ALLOC_GET_HEADER(Alloc_Arena_Header, data);
 	header->idx = 0;
-	if (ALLOC_UNLIKELY(header->next)) alloc_arenaFreeAll(header->next);
+	if (AIL_UNLIKELY(header->next)) ail_alloc_arena_free_all(header->next);
 	// @Decide: Should we memset previously allocated region to 0?
 }
 
--- ail_buf.h
+++ ail_buf.h
@@ -10,14 +10,13 @@
 #if  defined(AIL_MALLOC) &&  defined(AIL_FREE)
 #define AIL_BUF_MALLOC AIL_MALLOC
 #define AIL_BUF_FREE   AIL_FREE
-#endif
+#else
 #include <stdlib.h>
 #define AIL_BUF_FREE(ptr)    free(ptr)
 #define AIL_BUF_MALLOC(size) malloc(size)
+#endif
 #elif !defined(AIL_BUF_FREE) || !defined(AIL_BUF_MALLOC)
 #error "You must define both AIL_BUF_MALLOC and AIL_BUF_FREE, or neither."
-#elif !defined(UTIL_MALLOC)
-#define UTIL_MALLOC(size) AIL_BUF_MALLOC(size)
 #endif
 
 typedef struct {
@@ -25,41 +24,41 @@ typedef struct {
 	u64 idx;
 	u64 size;
 	u64 cap;
-} Buffer;
+} AIL_Buffer;
 
 // @TODO: Add overflow checks when reading/peeking
 
 #ifdef AIL_FS_H_
-Buffer buf_fromFile(const char *filename);
-bool buf_copyToFile(Buffer buf, const char *filename);
-bool buf_toFile(Buffer *buf, const char *filename);
+AIL_Buffer ail_buf_from_file(const char *filename);
+bool ail_buf_copy_to_file(AIL_Buffer buf, const char *filename);
+bool ail_buf_to_file(AIL_Buffer *buf, const char *filename);
 #endif // AIL_FS_H_
 
-Buffer buf_fromData(u8 *data, u64 size, u64 idx);
-Buffer buf_new(u64 cap);
-void buf_ensure_size(Buffer *buf, u64 n);
-void buf_free(Buffer buf);
-u8  buf_peek1   (Buffer  buf);
-u16 buf_peek2lsb(Buffer  buf);
-u32 buf_peek4lsb(Buffer  buf);
-u64 buf_peek8lsb(Buffer  buf);
-u16 buf_peek2msb(Buffer  buf);
-u32 buf_peek4msb(Buffer  buf);
-u64 buf_peek8msb(Buffer  buf);
-u8  buf_read1   (Buffer *buf);
-u16 buf_read2lsb(Buffer *buf);
-u32 buf_read4lsb(Buffer *buf);
-u64 buf_read8lsb(Buffer *buf);
-u16 buf_read2msb(Buffer *buf);
-u32 buf_read4msb(Buffer *buf);
-u64 buf_read8msb(Buffer *buf);
-void buf_write1   (Buffer *buf, u8  val);
-void buf_write2lsb(Buffer *buf, u16 val);
-void buf_write4lsb(Buffer *buf, u32 val);
-void buf_write8lsb(Buffer *buf, u64 val);
-void buf_write2msb(Buffer *buf, u16 val);
-void buf_write4msb(Buffer *buf, u32 val);
-void buf_write8msb(Buffer *buf, u64 val);
+AIL_Buffer ail_buf_from_data(u8 *data, u64 size, u64 idx);
+AIL_Buffer ail_buf_new(u64 cap);
+void ail_buf_ensure_size(AIL_Buffer *buf, u64 n);
+void ail_buf_free(AIL_Buffer buf);
+u8  ail_buf_peek1   (AIL_Buffer  buf);
+u16 ail_buf_peek2lsb(AIL_Buffer  buf);
+u32 ail_buf_peek4lsb(AIL_Buffer  buf);
+u64 ail_buf_peek8lsb(AIL_Buffer  buf);
+u16 ail_buf_peek2msb(AIL_Buffer  buf);
+u32 ail_buf_peek4msb(AIL_Buffer  buf);
+u64 ail_buf_peek8msb(AIL_Buffer  buf);
+u8  ail_buf_read1   (AIL_Buffer *buf);
+u16 ail_buf_read2lsb(AIL_Buffer *buf);
+u32 ail_buf_read4lsb(AIL_Buffer *buf);
+u64 ail_buf_read8lsb(AIL_Buffer *buf);
+u16 ail_buf_read2msb(AIL_Buffer *buf);
+u32 ail_buf_read4msb(AIL_Buffer *buf);
+u64 ail_buf_read8msb(AIL_Buffer *buf);
+void ail_buf_write1   (AIL_Buffer *buf, u8  val);
+void ail_buf_write2lsb(AIL_Buffer *buf, u16 val);
+void ail_buf_write4lsb(AIL_Buffer *buf, u32 val);
+void ail_buf_write8lsb(AIL_Buffer *buf, u64 val);
+void ail_buf_write2msb(AIL_Buffer *buf, u16 val);
+void ail_buf_write4msb(AIL_Buffer *buf, u32 val);
+void ail_buf_write8msb(AIL_Buffer *buf, u64 val);
 #endif // AIL_BUF_H_
 
 
@@ -67,11 +66,11 @@ void buf_write8msb(Buffer *buf, u64 val);
 #ifdef  AIL_BUF_IMPLEMENTATION
 
 #ifdef AIL_FS_H_
-Buffer buf_fromFile(const char *filename)
+AIL_Buffer ail_buf_from_file(const char *filename)
 {
 	u64 size;
 	u8 *data = (u8 *)ail_fs_read_file(filename, &size);
-	return (Buffer) {
+	return (AIL_Buffer) {
 		.data = data,
 		.idx  = 0,
 		.size = size,
@@ -79,22 +78,22 @@ Buffer buf_fromFile(const char *filename)
 	};
 }
 
-inline bool buf_copyToFile(Buffer buf, const char *filename)
+inline bool ail_buf_copy_to_file(AIL_Buffer buf, const char *filename)
 {
 	return ail_fs_write_file(filename, (char*) buf.data, buf.size);
 }
 
 // Unlike copyToFile, this function frees the buffer after writing it to the file
-bool buf_toFile(Buffer *buf, const char *filename)
+bool ail_buf_to_file(AIL_Buffer *buf, const char *filename)
 {
 	bool out = ail_fs_write_file(filename, (char*)(buf->data), buf->size);
 	AIL_BUF_FREE((void *)buf->data);
 	return out;
 }
 
-Buffer buf_new(u64 cap)
+AIL_Buffer ail_buf_new(u64 cap)
 {
-	Buffer buf = {
+	AIL_Buffer buf = {
 		.data = AIL_BUF_MALLOC(cap),
 		.size = 0,
 		.cap  = cap,
@@ -104,9 +103,9 @@ Buffer buf_new(u64 cap)
 }
 #endif // AIL_FS_H_
 
-inline Buffer buf_fromData(u8 *data, u64 size, u64 idx)
+inline AIL_Buffer ail_buf_from_data(u8 *data, u64 size, u64 idx)
 {
-	return (Buffer) {
+	return (AIL_Buffer) {
 		.data = data,
 		.idx  = idx,
 		.size = size,
@@ -114,13 +113,13 @@ inline Buffer buf_fromData(u8 *data, u64 size, u64 idx)
 	};
 }
 
-inline void buf_free(Buffer buf)
+inline void ail_buf_free(AIL_Buffer buf)
 {
 	AIL_BUF_FREE((void *)buf.data);
 }
 
 // Ensures that there's enough capacity to write `n` more bytes into the buffer
-void buf_ensure_size(Buffer *buf, u64 n)
+void ail_buf_ensure_size(AIL_Buffer *buf, u64 n)
 {
 	u64 min = buf->size + n;
 	if (AIL_UNLIKELY(min > buf->cap)) {
@@ -134,106 +133,106 @@ void buf_ensure_size(Buffer *buf, u64 n)
 	}
 }
 
-inline u8  buf_peek1(Buffer buf)
+inline u8 ail_buf_peek1(AIL_Buffer buf)
 {
 	return buf.data[buf.idx];
 }
-#define buf_peek1lsb(buf) buf_peek1(buf)
-#define buf_peek1msb(buf) buf_peek1(buf)
+#define ail_buf_peek1lsb(buf) ail_buf_peek1f)
+#define ail_buf_peek1msb(buf) ail_buf_peek1f)
 
-inline u16 buf_peek2lsb(Buffer buf)
+inline u16 ail_buf_peek2lsb(AIL_Buffer buf)
 {
 	return ((u16)buf.data[buf.idx + 1] << 8) | ((u16)buf.data[buf.idx + 0] << 0);
 }
 
-inline u32 buf_peek4lsb(Buffer buf)
+inline u32 ail_buf_peek4lsb(AIL_Buffer buf)
 {
 	return ((u32)buf.data[buf.idx + 3] << 24) | ((u32)buf.data[buf.idx + 2] << 16) | ((u32)buf.data[buf.idx + 1] << 8) | ((u32)buf.data[buf.idx + 0] << 0);
 }
 
-inline u64 buf_peek8lsb(Buffer buf)
+inline u64 ail_buf_peek8lsb(AIL_Buffer buf)
 {
 	return ((u64)buf.data[buf.idx + 7] << 56) | ((u64)buf.data[buf.idx + 6] << 48) | ((u64)buf.data[buf.idx + 5] << 40) | ((u64)buf.data[buf.idx + 4] << 32) |
 		   ((u64)buf.data[buf.idx + 3] << 24) | ((u64)buf.data[buf.idx + 2] << 16) | ((u64)buf.data[buf.idx + 1] <<  8) | ((u64)buf.data[buf.idx + 0] <<  0);
 }
 
-inline u16 buf_peek2msb(Buffer buf)
+inline u16 ail_buf_peek2msb(AIL_Buffer buf)
 {
 	return ((u16)buf.data[buf.idx + 0] << 8) | ((u16)buf.data[buf.idx + 1] << 0);
 }
 
-inline u32 buf_peek4msb(Buffer buf)
+inline u32 ail_buf_peek4msb(AIL_Buffer buf)
 {
 	return ((u32)buf.data[buf.idx + 0] << 24) | ((u32)buf.data[buf.idx + 1] << 16) | ((u32)buf.data[buf.idx + 2] << 8) | ((u32)buf.data[buf.idx + 3] << 0);
 }
 
-inline u64 buf_peek8msb(Buffer buf)
+inline u64 ail_buf_peek8msb(AIL_Buffer buf)
 {
 	return ((u64)buf.data[buf.idx + 0] << 56) | ((u64)buf.data[buf.idx + 1] << 48) | ((u64)buf.data[buf.idx + 2] << 40) | ((u64)buf.data[buf.idx + 3] << 32) |
 		   ((u64)buf.data[buf.idx + 4] << 24) | ((u64)buf.data[buf.idx + 5] << 16) | ((u64)buf.data[buf.idx + 6] <<  8) | ((u64)buf.data[buf.idx + 7] <<  0);
 }
 
-u8  buf_read1(Buffer *buf)
+u8  ail_buf_read1(AIL_Buffer *buf)
 {
 	return buf->data[buf->idx++];
 }
-#define buf_read1lsb(buf) buf_read1(buf)
-#define buf_read1msb(buf) buf_read1(buf)
+#define ail_buf_read1lsb(buf) ail_buf_read1(buf)
+#define ail_buf_read1msb(buf) ail_buf_read1(buf)
 
-u16 buf_read2lsb(Buffer *buf)
+u16 ail_buf_read2lsb(AIL_Buffer *buf)
 {
-	u16 out   = buf_peek2lsb(*buf);
+	u16 out   = ail_buf_peek2lsb(*buf);
 	buf->idx += sizeof(u16);
 	return out;
 }
 
-u32 buf_read4lsb(Buffer *buf)
+u32 ail_buf_read4lsb(AIL_Buffer *buf)
 {
-	u32 out   = buf_peek4lsb(*buf);
+	u32 out   = ail_buf_peek4lsb(*buf);
 	buf->idx += sizeof(u32);
 	return out;
 }
 
-u64 buf_read8lsb(Buffer *buf)
+u64 ail_buf_read8lsb(AIL_Buffer *buf)
 {
-	u64 out   = buf_peek8lsb(*buf);
+	u64 out   = ail_buf_peek8lsb(*buf);
 	buf->idx += sizeof(u64);
 	return out;
 }
 
-u16 buf_read2msb(Buffer *buf)
+u16 ail_buf_read2msb(AIL_Buffer *buf)
 {
-	u16 out   = buf_peek2msb(*buf);
+	u16 out   = ail_buf_peek2msb(*buf);
 	buf->idx += sizeof(u16);
 	return out;
 }
 
-u32 buf_read4msb(Buffer *buf)
+u32 ail_buf_read4msb(AIL_Buffer *buf)
 {
-	u32 out   = buf_peek4msb(*buf);
+	u32 out   = ail_buf_peek4msb(*buf);
 	buf->idx += sizeof(u32);
 	return out;
 }
 
-u64 buf_read8msb(Buffer *buf)
+u64 ail_buf_read8msb(AIL_Buffer *buf)
 {
-	u64 out   = buf_peek8msb(*buf);
+	u64 out   = ail_buf_peek8msb(*buf);
 	buf->idx += sizeof(u64);
 	return out;
 }
 
-void buf_write1(Buffer *buf, u8  val)
+void ail_buf_write1(AIL_Buffer *buf, u8  val)
 {
-	buf_ensure_size(buf, 2);
+	ail_buf_ensure_size(buf, 2);
 	buf->data[buf->idx++] = val;
 	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
 }
-#define buf_write1lsb(buf, val) buf_write1(buf, val)
-#define buf_write1msb(buf, val) buf_write1(buf, val)
+#define ail_buf_write1lsb(buf, val) ail_buf_write1(buf, val)
+#define ail_buf_write1msb(buf, val) ail_buf_write1(buf, val)
 
-void buf_write2lsb(Buffer *buf, u16 val)
+void ail_buf_write2lsb(AIL_Buffer *buf, u16 val)
 {
-	buf_ensure_size(buf, sizeof(u16));
+	ail_buf_ensure_size(buf, sizeof(u16));
 	for (u8 i = 0; i < (u8)sizeof(u16); i++) {
 		buf->data[buf->idx++] = (u8)(val & 0xff);
 		val >>= 8;
@@ -241,9 +240,9 @@ void buf_write2lsb(Buffer *buf, u16 val)
 	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
 }
 
-void buf_write4lsb(Buffer *buf, u32 val)
+void ail_buf_write4lsb(AIL_Buffer *buf, u32 val)
 {
-	buf_ensure_size(buf, sizeof(u32));
+	ail_buf_ensure_size(buf, sizeof(u32));
 	for (u8 i = 0; i < (u8)sizeof(u32); i++) {
 		buf->data[buf->idx++] = (u8)(val & 0xff);
 		val >>= 8;
@@ -251,9 +250,9 @@ void buf_write4lsb(Buffer *buf, u32 val)
 	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
 }
 
-void buf_write8lsb(Buffer *buf, u64 val)
+void ail_buf_write8lsb(AIL_Buffer *buf, u64 val)
 {
-	buf_ensure_size(buf, sizeof(u64));
+	ail_buf_ensure_size(buf, sizeof(u64));
 	for (u8 i = 0; i < (u8)sizeof(u64); i++) {
 		buf->data[buf->idx++] = (u8)(val & 0xff);
 		val >>= 8;
@@ -261,9 +260,9 @@ void buf_write8lsb(Buffer *buf, u64 val)
 	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
 }
 
-void buf_write2msb(Buffer *buf, u16 val)
+void ail_buf_write2msb(AIL_Buffer *buf, u16 val)
 {
-	buf_ensure_size(buf, sizeof(u16));
+	ail_buf_ensure_size(buf, sizeof(u16));
 	for (u8 i = 0; i < (u8)sizeof(u16); i++) {
 		buf->data[buf->idx++] = (u8)(val & (0xff << 8*(sizeof(u16) - 1)));
 		val <<= 8;
@@ -271,9 +270,9 @@ void buf_write2msb(Buffer *buf, u16 val)
 	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
 }
 
-void buf_write4msb(Buffer *buf, u32 val)
+void ail_buf_write4msb(AIL_Buffer *buf, u32 val)
 {
-	buf_ensure_size(buf, sizeof(u32));
+	ail_buf_ensure_size(buf, sizeof(u32));
 	for (u8 i = 0; i < (u8)sizeof(u32); i++) {
 		buf->data[buf->idx++] = (u8)(val & (0xff << 8*(sizeof(u32) - 1)));
 		val <<= 8;
@@ -281,9 +280,9 @@ void buf_write4msb(Buffer *buf, u32 val)
 	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
 }
 
-void buf_write8msb(Buffer *buf, u64 val)
+void ail_buf_write8msb(AIL_Buffer *buf, u64 val)
 {
-	buf_ensure_size(buf, sizeof(u64));
+	ail_buf_ensure_size(buf, sizeof(u64));
 	for (u8 i = 0; i < (u8)sizeof(u64); i++) {
 		buf->data[buf->idx++] = (u8)(val & ((u64)0xff << 8*(sizeof(u64) - 1)));
 		val <<= 8;
