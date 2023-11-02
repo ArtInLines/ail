9b144fc8ee17727d6378dde190a795032a2cd6ff
ArtInLines <linesinart@gmail.com>
2023-11-02 15:05:00 +0100

    fix: ail_buf write MSB fixed

--- ail_buf.h
+++ ail_buf.h
@@ -13,7 +13,7 @@
 #else
 #include <stdlib.h>
 #define AIL_BUF_FREE(ptr)    free(ptr)
-#define AIL_BUF_MALLOC(size) malloc(size)
+#define AIL_BUF_MALLOC(sze) malloc(sze)
 #endif
 #elif !defined(AIL_BUF_FREE) || !defined(AIL_BUF_MALLOC)
 #error "You must define both AIL_BUF_MALLOC and AIL_BUF_FREE, or neither."
@@ -22,7 +22,7 @@
 typedef struct {
 	u8 *data;
 	u64 idx;
-	u64 size;
+	u64 len;
 	u64 cap;
 } AIL_Buffer;
 
@@ -34,7 +34,7 @@ bool ail_buf_copy_to_file(AIL_Buffer buf, const char *filename);
 bool ail_buf_to_file(AIL_Buffer *buf, const char *filename);
 #endif // AIL_FS_H_
 
-AIL_Buffer ail_buf_from_data(u8 *data, u64 size, u64 idx);
+AIL_Buffer ail_buf_from_data(u8 *data, u64 len, u64 idx);
 AIL_Buffer ail_buf_new(u64 cap);
 void ail_buf_ensure_size(AIL_Buffer *buf, u64 n);
 void ail_buf_free(AIL_Buffer buf);
@@ -74,25 +74,25 @@ void ail_buf_write8msb(AIL_Buffer *buf, u64 val);
 #ifdef AIL_FS_H_
 AIL_Buffer ail_buf_from_file(const char *filename)
 {
-	u64 size;
-	u8 *data = (u8 *)ail_fs_read_file(filename, &size);
+	u64 len;
+	u8 *data = (u8 *)ail_fs_read_file(filename, &len);
 	return (AIL_Buffer) {
 		.data = data,
 		.idx  = 0,
-		.size = size,
-		.cap  = size,
+		.len  = len,
+		.cap  = len,
 	};
 }
 
 inline bool ail_buf_copy_to_file(AIL_Buffer buf, const char *filename)
 {
-	return ail_fs_write_file(filename, (char*) buf.data, buf.size);
+	return ail_fs_write_file(filename, (char*) buf.data, buf.len);
 }
 
 // Unlike copyToFile, this function frees the buffer after writing it to the file
 bool ail_buf_to_file(AIL_Buffer *buf, const char *filename)
 {
-	bool out = ail_fs_write_file(filename, (char*)(buf->data), buf->size);
+	bool out = ail_fs_write_file(filename, (char*)(buf->data), buf->len);
 	AIL_BUF_FREE((void *)buf->data);
 	return out;
 }
@@ -101,7 +101,7 @@ AIL_Buffer ail_buf_new(u64 cap)
 {
 	AIL_Buffer buf = {
 		.data = AIL_BUF_MALLOC(cap),
-		.size = 0,
+		.len  = 0,
 		.cap  = cap,
 		.idx  = 0
 	};
@@ -109,13 +109,13 @@ AIL_Buffer ail_buf_new(u64 cap)
 }
 #endif // AIL_FS_H_
 
-inline AIL_Buffer ail_buf_from_data(u8 *data, u64 size, u64 idx)
+inline AIL_Buffer ail_buf_from_data(u8 *data, u64 len, u64 idx)
 {
 	return (AIL_Buffer) {
 		.data = data,
 		.idx  = idx,
-		.size = size,
-		.cap  = size,
+		.len  = len,
+		.cap  = len,
 	};
 }
 
@@ -127,12 +127,12 @@ inline void ail_buf_free(AIL_Buffer buf)
 // Ensures that there's enough capacity to write `n` more bytes into the buffer
 void ail_buf_ensure_size(AIL_Buffer *buf, u64 n)
 {
-	u64 min = buf->size + n;
+	u64 min = buf->len + n;
 	if (AIL_UNLIKELY(min > buf->cap)) {
 		u64 new_cap = buf->cap * 2;
 		if (AIL_UNLIKELY(min > new_cap)) new_cap = min;
 		u8 *new_data = (u8 *) AIL_BUF_MALLOC(new_cap);
-		memcpy(new_data, buf->data, buf->size);
+		memcpy(new_data, buf->data, buf->len);
 		AIL_BUF_FREE(buf->data);
 		buf->data = new_data;
 		buf->cap = new_cap;
@@ -255,7 +255,7 @@ void ail_buf_write1(AIL_Buffer *buf, u8  val)
 {
 	ail_buf_ensure_size(buf, 2);
 	buf->data[buf->idx++] = val;
-	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
+	if (AIL_LIKELY(buf->idx > buf->len)) buf->len = buf->idx;
 }
 #define ail_buf_write1lsb(buf, val) ail_buf_write1(buf, val)
 #define ail_buf_write1msb(buf, val) ail_buf_write1(buf, val)
@@ -267,7 +267,7 @@ void ail_buf_write2lsb(AIL_Buffer *buf, u16 val)
 		buf->data[buf->idx++] = (u8)(val & 0xff);
 		val >>= 8;
 	}
-	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
+	if (AIL_LIKELY(buf->idx > buf->len)) buf->len = buf->idx;
 }
 
 void ail_buf_write3lsb(AIL_Buffer *buf, u32 val)
@@ -277,7 +277,7 @@ void ail_buf_write3lsb(AIL_Buffer *buf, u32 val)
 		buf->data[buf->idx++] = (u8)(val & 0xff);
 		val >>= 8;
 	}
-	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
+	if (AIL_LIKELY(buf->idx > buf->len)) buf->len = buf->idx;
 }
 
 void ail_buf_write4lsb(AIL_Buffer *buf, u32 val)
@@ -287,7 +287,7 @@ void ail_buf_write4lsb(AIL_Buffer *buf, u32 val)
 		buf->data[buf->idx++] = (u8)(val & 0xff);
 		val >>= 8;
 	}
-	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
+	if (AIL_LIKELY(buf->idx > buf->len)) buf->len = buf->idx;
 }
 
 void ail_buf_write8lsb(AIL_Buffer *buf, u64 val)
@@ -297,47 +297,43 @@ void ail_buf_write8lsb(AIL_Buffer *buf, u64 val)
 		buf->data[buf->idx++] = (u8)(val & 0xff);
 		val >>= 8;
 	}
-	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
+	if (AIL_LIKELY(buf->idx > buf->len)) buf->len = buf->idx;
 }
 
 void ail_buf_write2msb(AIL_Buffer *buf, u16 val)
 {
 	ail_buf_ensure_size(buf, sizeof(u16));
 	for (u8 i = 0; i < (u8)sizeof(u16); i++) {
-		buf->data[buf->idx++] = (u8)(val & (0xff << 8*(sizeof(u16) - 1)));
-		val <<= 8;
+		buf->data[buf->idx++] = (u8)(0xff & (val >> 8*(sizeof(u16) - i - 1)));
 	}
-	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
+	if (AIL_LIKELY(buf->idx > buf->len)) buf->len = buf->idx;
 }
 
 void ail_buf_write3msb(AIL_Buffer *buf, u32 val)
 {
 	ail_buf_ensure_size(buf, 3);
 	for (u8 i = 0; i < 3; i++) {
-		buf->data[buf->idx++] = (u8)(val & (0xff << 8*(3 - 1)));
-		val <<= 8;
+		buf->data[buf->idx++] = (u8)(0xff & (val >> 8*(3 - i - 1)));
 	}
-	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
+	if (AIL_LIKELY(buf->idx > buf->len)) buf->len = buf->idx;
 }
 
 void ail_buf_write4msb(AIL_Buffer *buf, u32 val)
 {
 	ail_buf_ensure_size(buf, sizeof(u32));
 	for (u8 i = 0; i < (u8)sizeof(u32); i++) {
-		buf->data[buf->idx++] = (u8)(val & (0xff << 8*(sizeof(u32) - 1)));
-		val <<= 8;
+		buf->data[buf->idx++] = (u8)(0xff & (val >> 8*(sizeof(u32) - i - 1)));
 	}
-	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
+	if (AIL_LIKELY(buf->idx > buf->len)) buf->len = buf->idx;
 }
 
 void ail_buf_write8msb(AIL_Buffer *buf, u64 val)
 {
 	ail_buf_ensure_size(buf, sizeof(u64));
 	for (u8 i = 0; i < (u8)sizeof(u64); i++) {
-		buf->data[buf->idx++] = (u8)(val & ((u64)0xff << 8*(sizeof(u64) - 1)));
-		val <<= 8;
+		buf->data[buf->idx++] = (u8)(0xff & (val >> 8*(sizeof(u64) - i - 1)));
 	}
-	if (AIL_LIKELY(buf->idx > buf->size)) buf->size = buf->idx;
+	if (AIL_LIKELY(buf->idx > buf->len)) buf->len = buf->idx;
 }
 
 
