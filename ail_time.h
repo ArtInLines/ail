bcbf2d0d44c4d37d6edb70055ee40e5263bf34fa
ArtInLines <linesinart@gmail.com>
2024-02-22 22:15:04 +0100

    Add timing functionality to ail_time.h

--- ail_time.h
+++ ail_time.h
@@ -29,6 +29,8 @@ SOFTWARE.
 #define AIL_TYPES_IMPL
 #include "ail.h"
 
+#include <time.h> // For clock
+
 #ifndef AIL_TIME_DEF
 #ifdef  AIL_DEF
 #define AIL_TIME_DEF AIL_DEF
@@ -45,7 +47,11 @@ SOFTWARE.
 #endif // AIL_TIME_DEF_INLINE
 
 // Sleep `msecs` milliseconds
-void ail_time_sleep(u64 msecs);
+AIL_TIME_DEF void ail_time_sleep(u64 msecs);
+
+// Time in seconds here
+AIL_TIME_DEF_INLINE f64 ail_time_clock_start(void);
+AIL_TIME_DEF_INLINE f64 ail_time_clock_elapsed(f64 start);
 
 #endif // AIL_TIME_H_
 
@@ -56,30 +62,49 @@ void ail_time_sleep(u64 msecs);
 
 #ifdef WIN32
 #include <windows.h> // For Sleep
-#define AIL_TIME_WINSLEEP
+#define AIL_TIME_FLAG_WINSLEEP
 #elif _POSIX_C_SOURCE >= 199309L
-#define AIL_TIME_NANOSLEEP
+#define AIL_TIME_FLAG_NANOSLEEP
 #include <time.h>    // for nanosleep
 #else
-#define AIL_TIME_USLEEP
+#define AIL_TIME_FLAG_USLEEP
 #include <unistd.h>  // for usleep
 #endif
 
 void ail_time_sleep(u64 msecs)
 {
-#ifdef AIL_TIME_WINSLEEP
-    Sleep(msecs);
-#elif AIL_TIME_NANOSLEEP
+#if defined(AIL_TIME_FLAG_NANOSLEEP)
     struct timespec ts;
     ts.tv_sec = msecs / 1000;
     ts.tv_nsec = (msecs % 1000) * 1000000;
     nanosleep(&ts, NULL);
-#elif AIL_TIME_USLEEP
+#elif defined(AIL_TIME_FLAG_WINSLEEP)
+    Sleep(msecs);
+#elif defined(AIL_TIME_FLAG_USLEEP)
     if (msecs >= 1000) sleep(msecs / 1000);
     usleep((msecs % 1000) * 1000);
 #else
 #	error "Missing feature flag not implemented yet"
 #endif
+}
+
+f64 ail_time_clock_start(void)
+{
+#ifdef _WIN32
+    return (f64)timeGetTime() / 1000;
+#elif __posix
+    struct timespec ts = {0};
+    int ret = clock_gettime(CLOCK_MONOTONIC, &ts);
+    AIL_ASSERT(ret == 0);
+    return (f64)ts.tv_sec + ts.tv_nsec*1e-9;
+#else
+    return (f64)clock() / CLOCKS_PER_SEC;
+#endif
+}
+
+f64 ail_time_clock_elapsed(f64 start)
+{
+    return ail_time_clock_start() - start;
 }
 
 
