fbfc9edee403166417320308685b5a83c52d51d4
ArtInLines <64645307+ArtInLines@users.noreply.github.com>
2024-05-16 22:38:28 +0200

    add macro-overloading based on argument amount

--- ail.h
+++ ail.h
@@ -190,6 +190,29 @@ typedef char*    str;
     #define AIL_PACK_END()
 #endif
 
+// To allow overloading macros based on number of arguments (taken from here: https://stackoverflow.com/a/26408195/13764271)
+#define __AIL_RSEQ_N() \
+     63,62,61,60,                   \
+     59,58,57,56,55,54,53,52,51,50, \
+     49,48,47,46,45,44,43,42,41,40, \
+     39,38,37,36,35,34,33,32,31,30, \
+     29,28,27,26,25,24,23,22,21,20, \
+     19,18,17,16,15,14,13,12,11,10, \
+     9,8,7,6,5,4,3,2,1,0
+#define __AIL_ARG_N( \
+      _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
+     _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+     _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+     _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
+     _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
+     _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
+     _61,_62,_63,N,...) N
+#define __AIL_NARG_I_(...) __AIL_ARG_N(__VA_ARGS__)
+#define __AIL_NARG__(...)  __AIL_NARG_I_(__VA_ARGS__,__AIL_RSEQ_N())
+#define _AIL_VFUNC_(name, n) name##n
+#define _AIL_VFUNC(name, n) _AIL_VFUNC_(name, n)
+#define AIL_VFUNC(func, ...) _AIL_VFUNC(func, __AIL_NARG__(__VA_ARGS__)) (__VA_ARGS__)
+
 #define _AIL_STRINGIZE2(x) #x
 #define AIL_STRINGIZE(x) _AIL_STRINGIZE2(x)
 #define AIL_STR_LINE AIL_STRINGIZE(__LINE__)
@@ -242,17 +265,19 @@ typedef char*    str;
 
 // @TODO: Better static assert message
 #ifdef __cpp_static_assert
-    #define AIL_STATIC_ASSERT(cond, msg) static_assert(!!(cond), msg)
+    #define _AIL_STATIC_ASSERT2(cond, msg) static_assert(!!(cond), msg)
 #elif __STDC_VERSION__ >= 202311L
-    #define AIL_STATIC_ASSERT(cond, msg) static_assert(!!(cond), msg)
+    #define _AIL_STATIC_ASSERT2(cond, msg) static_assert(!!(cond), msg)
 #elif __STDC_VERSION__ >= 201112L
     #include <assert.h>
-    #define AIL_STATIC_ASSERT(cond, msg) _Static_assert(!!(cond), msg)
+    #define _AIL_STATIC_ASSERT2(cond, msg) _Static_assert(!!(cond), msg)
 #else
-    #define _AIL_STATIC_ASSERT2(cond, msg, line) do { char ail_static_assertion_at_line##line[((!!(cond))*(AIL_ARRLEN(msg)+2))-1] = msg; AIL_UNUSED(ail_static_assertion_at_line##line); } while(0)
-    #define _AIL_STATIC_ASSERT1(cond, msg, line) _AIL_STATIC_ASSERT2(cond, msg, line)
-    #define AIL_STATIC_ASSERT(cond, msg)         _AIL_STATIC_ASSERT1(cond, msg, __LINE__)
+    #define _AIL_STATIC_ASSERT_MSG2(cond, msg, line) do { char ail_static_assertion_at_line##line[((!!(cond))*(AIL_ARRLEN(msg)+2))-1] = msg; AIL_UNUSED(ail_static_assertion_at_line##line); } while(0)
+    #define _AIL_STATIC_ASSERT_MSG1(cond, msg, line) _AIL_STATIC_ASSERT_MSG2(cond, msg, line)
+    #define _AIL_STATIC_ASSERT2(cond, msg)           _AIL_STATIC_ASSERT_MSG1(cond, msg, __LINE__)
 #endif
+#define _AIL_STATIC_ASSERT1(cond) _AIL_STATIC_ASSERT2(cond, __FILE__ ":" AIL_STR_LINE ": Static Assert failed")
+#define AIL_STATIC_ASSERT(...) AIL_VFUNC(_AIL_STATIC_ASSERT, __VA_ARGS__)
 
 #define AIL_OFFSETOF(var, field) (((char *) &(var)->field) - ((char *) (var)))
 
