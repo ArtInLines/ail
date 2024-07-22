6fd8b0281f51a302c5918f410ca20fee79ba2efd
ArtInLines <64645307+ArtInLines@users.noreply.github.com>
2024-07-22 23:18:23 +0200

    add List template alongside dynamic-arrays in ail.h

--- ail.h
+++ ail.h
@@ -780,30 +780,38 @@ AIL_DEF void __ail_default_allocator_unused__(void)
 #endif
 
 #ifdef AIL_TYPES_IMPL
+#define AIL_LIST_INIT(T) typedef struct AIL_LIST_##T { T *data; u32 len; } AIL_LIST_##T
+#define AIL_LIST(T) AIL_LIST_##T
 #define AIL_DA_INIT(T) typedef struct AIL_DA_##T { T *data; u32 len; u32 cap; AIL_Allocator allocator; } AIL_DA_##T
 #define AIL_DA(T) AIL_DA_##T
-AIL_DA_INIT(u8);
-AIL_DA_INIT(u16);
-AIL_DA_INIT(u32);
-AIL_DA_INIT(u64);
-AIL_DA_INIT(i8);
-AIL_DA_INIT(i16);
-AIL_DA_INIT(i32);
-AIL_DA_INIT(i64);
-AIL_DA_INIT(f32);
-AIL_DA_INIT(f64);
-AIL_DA_INIT(str);
+AIL_DA_INIT(u8);   AIL_LIST_INIT(u8);
+AIL_DA_INIT(u16);  AIL_LIST_INIT(u16);
+AIL_DA_INIT(u32);  AIL_LIST_INIT(u32);
+AIL_DA_INIT(u64);  AIL_LIST_INIT(u64);
+AIL_DA_INIT(i8);   AIL_LIST_INIT(i8);
+AIL_DA_INIT(i16);  AIL_LIST_INIT(i16);
+AIL_DA_INIT(i32);  AIL_LIST_INIT(i32);
+AIL_DA_INIT(i64);  AIL_LIST_INIT(i64);
+AIL_DA_INIT(f32);  AIL_LIST_INIT(f32);
+AIL_DA_INIT(f64);  AIL_LIST_INIT(f64);
+AIL_DA_INIT(str);  AIL_LIST_INIT(str);
 #else
+#define AIL_LIST_INIT(T) typedef struct AIL_LIST_##T { T *data; unsigned int len; } AIL_LIST_##T
+#define AIL_LIST(T) AIL_LIST_##T
 #define AIL_DA_INIT(T) typedef struct AIL_DA_##T { T *data; unsigned int len; unsigned int cap; AIL_Allocator *allocator; } AIL_DA_##T
 #define AIL_DA(T) AIL_DA_##T
 #endif
 
-AIL_DA_INIT(void);
-AIL_DA_INIT(char);
-#endif // _AIL_DA_GUARD_
-#endif // AIL_DA_IMPL
+AIL_DA_INIT(void); AIL_LIST_INIT(void);
+AIL_DA_INIT(char); AIL_LIST_INIT(char);
 
-// #define AIL_DA(T) AIL_DA // To allow adding the element T in array definitions - serves only as documentation
+
+#define ail_list_from_parts(d, l) { .data = (d),       .len = (l) }
+#define ail_list_from_arr(arr)    { .data = (arr),     .len = AIL_ARRLEN(arr) }
+#define ail_list_from_da(da)      { .data = (da).data, .len = (da).len }
+#define ail_list_from_parts_t(T, d, l) (AIL_LIST(T))ail_list_from_parts(d, l)
+#define ail_list_from_arr_t(T, arr)    (AIL_LIST(T))ail_list_from_arr(arr)
+#define ail_list_from_da_t(T, da)      (AIL_LIST(T))ail_list_from_da(da)
 
 #define ail_da_from_parts(d, l, c, al)  { .data = (d), .len = (l), .cap = (c), .allocator = (al) }
 #define ail_da_new_with_alloc(T, c, al) { .data = AIL_CALL_ALLOC ((al), sizeof(T)*(c)), .len = 0, .cap = (c), .allocator = (al) }
@@ -917,5 +925,8 @@ AIL_DA_INIT(char);
     } while(0)
 
 #define ail_da_rm_swap(daPtr, idx) (daPtr)->data[(idx)] = (daPtr)->data[--(daPtr)->len]
+
+#endif // _AIL_DA_GUARD_
+#endif // AIL_DA_IMPL
 
 #endif // AIL_H_
